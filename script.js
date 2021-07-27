f200b90c5a3f686330cf6cf401a37038c5ed286a
sabare47 <42890093+sabare47@users.noreply.github.com>
2021-07-27 08:19:11 +0530

    with road

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 const startscreen = document.querySelector(".startscreen");//declaration for startscreen
-const gamescreen  = document.querySelector(".gamescreen");//decleration for gamescreen
+//const gamescreen  = document.querySelector(".gamescreen");//decleration for gamescreen
 const score = document.querySelector(".score");//decleration for score area
 const gamearea = document.querySelector(".gamearea");//decleration for gamearea
 startscreen.addEventListener("click",run_game);//adding eventlistener for startscreen
 
-document.addEventListener("keydown",movement);//adding function to monitor keypressing
+document.addEventListener("keydown",presson);//adding function to monitor keypressing
+document.addEventListener("keyup",pressoff);
+let player = {speed:5}
 let direction = {
-    ArrowLeft:"moveleft",
-    ArrowRight:"moveright",
-    ArrowUp:"moveup",
-    ArrowDown:"movedown"
+    ArrowLeft:false,
+    ArrowRight:false,
+    ArrowUp:false,
+    ArrowDown:false
+}
+function play_game() {
+    let car = document.querySelector(".car");
+    if (player.start) {
+        if (direction.ArrowDown) {player.y += player.speed;}
+        if (direction.ArrowUp) {player.y -= player.speed;}
+        if (direction.ArrowLeft) {player.x -= player.speed;}
+        if (direction.ArrowRight) {player.x += player.speed;}
+        car.style.left = player.x + 'px';
+        car.style.top = player.y + 'px';
+
+        window.requestAnimationFrame(play_game);
+    }
 }
 function run_game() {
     startscreen.classList.add("hide");//hide the startscreen
-    gamescreen.classList.remove("hide");//show gamescreen
+    gamearea.classList.remove("hide");//show gamescreen
+    player.start =  true;
+    window.requestAnimationFrame(play_game);
+    let car = document.createElement("div");
+    car.innerText =  "car";
+    car.setAttribute("class","car");
+    gamearea.appendChild(car);
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
 }
 
-function movement(e){
-    let keys = direction[e.key];
-    console.log(keys);
+function presson(e) {
+    direction[e.key] = true;
+}
+
+function pressoff(e) {
+    direction[e.key] = false;
 }
